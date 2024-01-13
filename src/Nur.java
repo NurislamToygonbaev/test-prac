19ca2c7349454e1c6f8847b3dcb763ebb184aa03
nurkamil <nurkamilkamchiev@gmail.com>
2024-01-13 15:21:04 +0600

    add closer

--- src/Nur.java
+++ src/Nur.java
@@ -1,6 +1,6 @@
 public class Nur{
 	public void method(){
 		System.out.println("hello java " + 10 + 2);
-		System.out.println("Hello my name is Nurkamil")
+		System.out.println("Hello my name is Nurkamil");
 	}
 }
