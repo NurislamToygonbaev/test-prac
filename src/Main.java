c3c018d6d36614634b70cd7290f3c05d2a9eb6ad
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2024-01-13 15:44:17 +0600

    ali

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Nurkamil");
+        System.out.println("Temirbekov Aliaskar");
+        System.out.println("Welcome to Java 12");
+
+
+        Ali ali = new Ali();
+        ali.boo();
+        Nur nur = new Nur();
+        nur.method();
     }
 }
