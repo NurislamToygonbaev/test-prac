6f632fa831ea3c4cedea709140dffea23428f69d
Notnik_kg <aliaskartemirbekov@gmail.com>
2024-01-13 11:23:13 +0300

    Like

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Temirbekov Aliaskar");
+        System.out.println("Welcome to Java 12");
     }
 }
