a2ce22aa802b4e1b78d0b709c14b9c7201e804a5
Notnik_kg <aliaskartemirbekov@gmail.com>
2024-01-13 12:15:15 +0300

    Nurislam

--- src/Ali.java
+++ src/Ali.java
@@ -1,7 +1,14 @@
 public class Ali{
     public void boo(){
        System.out.println("Good live!");
-  System.out.print("Bishkek"); }
+  System.out.print("Bishkek");
+  for(int i = 0;i<5;i++){
+  System.out.print("Nurislam");
+
+  }
+
+    }
+
 
 
 }
